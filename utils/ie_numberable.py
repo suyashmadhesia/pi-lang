6b99af71c136ab3a21996cc9e2854c77c485a561
Suyash Maddhessiya <suyashmadhesia@gmail.com>
2023-09-08 10:08:07 +0530

    added type hints for IEnumerable class

--- utils/ie_numberable.py
+++ utils/ie_numberable.py
@@ -1,12 +1,12 @@
 class IEnumerable:
-    def __init__(self, data):
+    def __init__(self, data: any) -> None:
         self.data = data
         self.index = 0
 
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def __next__(self) -> int:
         if self.index < len(self.data):
             result = self.data[self.index]
             self.index += 1
